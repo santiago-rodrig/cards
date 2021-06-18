d96716a31495c56a1a9c8ec27b63bbbb7ff7f8db
Santiago Rodriguez <santo1996.29@gmail.com>
2021-06-18 09:43:56 -0400

    deck initializer function

--- deck.go
+++ deck.go
@@ -6,6 +6,31 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() (cards deck) {
+	cardSuits := []string{"Spades", "Hearts", "Diamonds"}
+	cardValues := []string{
+		"Ace",
+		"Two",
+		"Three",
+		"Four",
+		"Five",
+		"Six",
+		"Seven",
+		"Eight",
+		"Nine",
+		"Ten",
+		"Jack",
+		"Queen",
+		"King",
+	}
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return
+}
+
 func (d deck) print() {
 	for i, v := range d {
 		fmt.Println(i, v)
--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Spades", "Five of Diamonds", "Six of Spades"}
+	cards := newDeck()
 	cards.print()
 }
