fbfc9a5dc4cc05a5a070c238ab146f2961f92480
Santiago Rodriguez <santo1996.29@gmail.com>
2021-06-19 10:28:46 -0400

    start from scratch

--- main.go
+++ main.go
@@ -3,15 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	cards := newDeck()
-	Must(cards.saveToFile("cards.txt"))
-	cards, err := newDeckFromFile("cards.txt")
-	Must(err)
-	fmt.Println(cards)
-}
-
-func Must(err error) {
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("Not implemented")
 }
