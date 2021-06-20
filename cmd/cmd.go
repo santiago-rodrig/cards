15ad0fd1d419aec360529871f351da5f9d1468da
Santiago Rodriguez <santo1996.29@gmail.com>
2021-06-19 20:41:44 -0400

    add executable command

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/santiago-rodrig/cards"
+	"os"
+	"strconv"
+)
+
+const notEnoughArgsMsg = "not enough arguments"
+const badArgMsg = "invalid argument, must be an integer greater than zero"
 
 func main() {
-	fmt.Println("Not implemented")
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		printError(notEnoughArgsMsg)
+		printUsage()
+		os.Exit(1)
+	}
+
+	arg, err := strconv.Atoi(flag.Arg(0))
+
+	if err != nil {
+		printError(badArgMsg)
+		printUsage()
+		os.Exit(1)
+	}
+
+	deck := cards.NewDeck()
+
+	for i := 0; i < arg; i++ {
+		card, err := deck.PickRandomCard()
+		if err != nil {
+			break
+		}
+		fmt.Println(card)
+	}
+}
+
+func printError(message string) {
+	fmt.Println("ERROR:", message)
+}
+
+func printUsage() {
+	fmt.Println("cards numberOfCards")
 }
